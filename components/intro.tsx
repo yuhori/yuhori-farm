e779275209afbd6792589b0765394eca14050f03
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2023-02-25 14:03:17 +0900

    修正

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,17 +4,18 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        ゆーほり農園
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        {/* A statically generated blog example using{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
           Next.js
         </a>{' '}
-        and {CMS_NAME}.
+        and {CMS_NAME}. */}
+        育てて食べて、満たされて
       </h4>
     </section>
   )
